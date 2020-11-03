a554a71340d5e785965d668551da944a053440eb
pauldab <pauldab@gmail.com>
2020-11-04 00:32:40 +0100

    Overriding GetLetterGrade @ RankedGradeBook.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,15 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return 'F';
+        }
     }
 }
